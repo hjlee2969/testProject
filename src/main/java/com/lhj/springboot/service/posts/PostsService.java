9d7f3308b217b8196122e7d1c778b2d0cddea171
이효재 <hjlee2969@naver.com>
2022-06-03 11:59:08 +0900

    !여섯번째 커밋 1.PostsApiController 생성   *lombok으로 @RequiredArgsConstructor를 추가하면    생성자로 postsService를 받을 수 있다. 단 꼭 private final 을 해야한다. 2.PostsApiControllerTest 작성

---
+++ src/main/java/com/lhj/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.lhj.springboot.service.posts;
+
+import com.lhj.springboot.domain.PostsRepository;
+import com.lhj.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
