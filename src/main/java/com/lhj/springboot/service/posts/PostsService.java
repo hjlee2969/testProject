c1681d2a6e85caa89354a0b7e38b589e36411d18
이효재 <hjlee2969@naver.com>
2022-06-06 16:19:06 +0900

    !열한두째 커밋(index페이지에 Posts 목록을 불러오기) 1.화면(index.mustache) 수정 2.PostsRepository.java -> List<Posts> findAllDesc() 쿼리 구현 3.PostsService.java List<PostsListResponseDto> findAllDesc() 구현 4.entity를 받아 줄 PostsListResponseDto 구현 5.IndexController에 "/" URL Mapper에 Model 등록

--- src/main/java/com/lhj/springboot/service/posts/PostsService.java
+++ src/main/java/com/lhj/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.lhj.springboot.service.posts;
 
 import com.lhj.springboot.domain.posts.Posts;
 import com.lhj.springboot.domain.posts.PostsRepository;
+import com.lhj.springboot.web.dto.PostsListResponseDto;
 import com.lhj.springboot.web.dto.PostsResponseDto;
 import com.lhj.springboot.web.dto.PostsSaveRequestDto;
 import com.lhj.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDecs().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
--- src/main/java/com/lhj/springboot/web/IndexController.java
+++ src/main/java/com/lhj/springboot/web/IndexController.java
@@ -1,13 +1,23 @@
 package com.lhj.springboot.web;
 
+import com.lhj.springboot.domain.posts.Posts;
+import com.lhj.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
