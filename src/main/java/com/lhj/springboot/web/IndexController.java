e6cb56e02c1a7a7c50456fd751df720666792576
이효재 <hjlee2969@naver.com>
2022-06-10 12:21:36 +0900

    #######문제 해결못함######## 스프링시큐리티 사용 위해 작성, 로그인 이후 user 정보를 불러오지 못함

--- src/main/java/com/lhj/springboot/web/IndexController.java
+++ src/main/java/com/lhj/springboot/web/IndexController.java
@@ -3,6 +3,7 @@ package com.lhj.springboot.web;
 import com.lhj.springboot.domain.posts.Posts;
 import com.lhj.springboot.service.posts.PostsService;
 import com.lhj.springboot.web.dto.PostsResponseDto;
+import config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,10 +20,18 @@ import java.util.List;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            System.out.println("user exist!!!");
+            model.addAttribute("userName", user.getName());
+        } else {
+            System.out.println("user not exist!!!");
+        }
         return "index";
     }
 
